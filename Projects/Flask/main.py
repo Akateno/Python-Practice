5ce7094b77f8fbc314ce5dbbea360d9f0712bfdb
Erick Vargas <erickkvargas@gmail.com>
2023-08-10 08:50:13 -0700

    docorator functions

---
+++ Projects/Flask/main.py
@@ -0,0 +1,61 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def hello_world():
+    return '<h1 style="text-align: center">Hello, World!</h1>' \
+            '<p>This is a paragraph</p> '\
+            '<img src="https://butwhytho.net/wp-content/uploads/2023/07/Gear-5-But-Why-Tho.jpg" >'\
+            '<img src="https://i.gifer.com/origin/e2/e2ffc3e709d4d3c376f6d4155bd0777b.gif" width=200 >'
+
+@app.route('/bye')
+def bye():
+    return "Bye User"
+
+@app.route('/username/<name>/<int:number>')
+def greet(name, number):
+    return f"Hello person named: {name}! you are {number} years old!"
+
+
+#turn on auto refresh (debug=True)
+if __name__== "__main__":
+    app.run(debug=True)
+
+#make CSS decorators 
+def make_bold(function): 
+    def wrapper():
+        return "<b>" + function() + "</b>"
+    return wrapper 
+
+def make_emphasis(function):
+    def wrapper():
+        return "<em>" + function() + "</em>"
+    return wrapper
+
+def make_underlined(function):
+    def wrapper():
+        return "<u>" + function() + "</u>"
+    return wrapper 
+
+
+#make advanced decorators 
+
+class User: 
+    def __init__(self,name):
+        self.name=name
+        self.is_logged_in = False
+
+def is_authenticated_decorator(function):
+    def wrapper(*args,**kwargs):
+        if args[0].is_logged_in == True:
+            function(args[0])
+    return wrapper 
+
+@is_authenticated_decorator
+def create_blog_post(user):
+    print(f"thiis is {user.name}s new blog post")
+
+new_user=User("Erick")
+new_user.is_logged_in=True 
